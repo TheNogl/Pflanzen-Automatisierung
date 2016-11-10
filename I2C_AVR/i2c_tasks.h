8fc4ca4b458ba6c495ebd5ab5a622c54732abb7a
TheNogl <nagelmaier@gmail.com>
2016-11-10 15:28:42 +0100

    Update i2c_tasks.h

--- I2C_AVR/i2c_tasks.h
+++ I2C_AVR/i2c_tasks.h
@@ -1,23 +1,23 @@
-#define MOISTURE 0x01
-#define TEMP 0x02
-#define AIRMOISTURE 0x03
-#define LUMINOSITY_RED 0x04
-#define LUMINOSITY_BLUE 0x05
-#define TIME_WATER_D 0x06
-#define TIME_WATER_H 0x07
-#define TIME_WATER_M 0x08
-#define TIME_START_D 0x09
-#define TIME_START_H 0x0A
-#define TIME_START_M 0x0B
-#define CHANGE_TEMP_1 0x0C
-#define CHANGE_TEMP_NEG1 0x0D
-#define CHANGE_TEMP_5 0x0E
-#define CHANGE_TEMP_NEG5 0x0F
-#define CHANGE_LUMINOSITY_RED_1 0x10
-#define CHANGE_LUMINOSITY_RED_NEG1 0x11
-#define CHANGE_LUMINOSITY_RED_5 0x12
-#define CHANGE_LUMINOSITY_RED_NEG5 0x13
-#define CHANGE_LUMINOSITY_BLUE_1 0x14
-#define CHANGE_LUMINOSITY_BLUE_NEG1 0x15
-#define CHANGE_LUMINOSITY_BLUE_5 0x16
-#define CHANGE_LUMINOSITY_BLUE_NEG5 0x17
+#define MOISTURE 0x01 //Answer: ADC-Measurement
+#define TEMP 0x02 //Answer: ADC-Measurement
+#define AIRMOISTURE 0x03 //Answer: Oszillator Frequency/Nominal Frequency*128
+#define LUMINOSITY_RED 0x04 //Answer: Percent 0-100
+#define LUMINOSITY_BLUE 0x05 //Answer: Percent 0-100
+#define TIME_WATER_D 0x06 //Answer: Time since last watering days
+#define TIME_WATER_H 0x07 //Answer: Time since last watering hours
+#define TIME_WATER_M 0x08 //Answer: Time since last watering minutes
+#define TIME_START_D 0x09 //Answer: Time since startup days
+#define TIME_START_H 0x0A //Answer: Time since startup hours
+#define TIME_START_M 0x0B //Answer: Time since startup minutes
+#define CHANGE_TEMP_1 0x0C //Answer: 0xAA
+#define CHANGE_TEMP_NEG1 0x0D //Answer: 0xAB
+#define CHANGE_TEMP_5 0x0E //Answer: 0xAC
+#define CHANGE_TEMP_NEG5 0x0F //Answer: 0xAD
+#define CHANGE_LUMINOSITY_RED_1 0x10 //Answer: 0xAE
+#define CHANGE_LUMINOSITY_RED_NEG1 0x11 //Answer: 0xAF
+#define CHANGE_LUMINOSITY_RED_5 0x12 //Answer: 0xB1
+#define CHANGE_LUMINOSITY_RED_NEG5 0x13 //Answer: 0xB2
+#define CHANGE_LUMINOSITY_BLUE_1 0x14 //Answer: 0xB3
+#define CHANGE_LUMINOSITY_BLUE_NEG1 0x15 //Answer: 0xB4
+#define CHANGE_LUMINOSITY_BLUE_5 0x16 //Answer: 0xB5
+#define CHANGE_LUMINOSITY_BLUE_NEG5 0x17 //Answer: 0xB6
